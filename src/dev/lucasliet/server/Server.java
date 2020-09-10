43f63c81b3b6d736a533bc2b642fe6d4f36711b2
lucasliet <lucasouliveira@gmail.com>
2020-09-10 11:19:32 -0300

    server done

---
+++ src/dev/lucasliet/server/Server.java
@@ -0,0 +1,52 @@
+package dev.lucasliet.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Server {
+	private AtomicBoolean running;
+	private ServerSocket server;
+	private ExecutorService threadPool;
+	
+	public Server() throws IOException {
+		this.running = new AtomicBoolean(true);
+		this.server = new ServerSocket(3333);
+		this.threadPool = Executors.newCachedThreadPool();		
+	}
+	
+	public void run() throws IOException {
+		while(this.running.get()) {
+			try {
+				Socket socket = this.server.accept();
+				System.out.println("New Client on port: " + socket.getPort());
+
+				TaskDeliver tasks = new TaskDeliver(threadPool, socket, this);
+
+				this.threadPool.execute(tasks);
+			} catch (SocketException e) {
+				if (!this.running.get())
+					System.out.println("Server isn't running");
+			}
+		}
+	}
+	
+	public void stop() throws IOException {
+		this.running.set(false);
+		this.threadPool.shutdown();
+		this.server.close();
+		System.out.println("--- Stopping Server ---");		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("--- Starting Server ---");	
+		
+		
+		
+	}
+
+}
---
+++ src/dev/lucasliet/server/TaskDeliver.java
@@ -0,0 +1,129 @@
+package dev.lucasliet.server;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class TaskDeliver implements Runnable {
+
+	private ExecutorService threadPool;
+	private Socket socket;
+	private Server server;
+
+	public TaskDeliver(ExecutorService threadPool, Socket socket, Server server) {
+		this.threadPool = threadPool;
+		this.socket = socket;
+		this.server = server;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.printf("=== Delivering tasks to client %s === %n" , socket);
+			Scanner clientInput = new Scanner(socket.getInputStream());
+			PrintStream clientOutput = new PrintStream(socket.getOutputStream());
+			
+			while(clientInput.hasNextLine()) {
+				String command = clientInput.nextLine();
+				
+				switch (command) {
+				
+					case "c1": {						
+						clientOutput.println("command c1 is running");
+		                this.threadPool.execute( () -> {
+		                	System.out.println("running command c1"); 
+
+		            		try {
+		            			Thread.sleep(20000); //task time simulation
+		            		} catch (InterruptedException e) {
+		            			throw new RuntimeException(e);
+		            		} 
+		            		
+		            		clientOutput.println("Server finished c1 command");
+		                });
+						break;
+					}
+					
+					case "c2": {
+						clientOutput.println("Confirmação do comando c2");
+	
+				        Future<String> dbResponse = this.threadPool.submit(() -> {
+				        	System.out.println("Server received c2 command - DB");
+				    		clientOutput.println("Processing c2 command - DB");
+
+				    		Thread.sleep(15000); //task time simulation
+
+				    		int num = new Random().nextInt(100) + 1; //number from 1 to 100
+				    		System.out.println("Server finished c2 command - DB");
+				    		return Integer.toString(num);
+				        });
+				        
+				        Future<String> apiResponse = this.threadPool.submit(() -> {
+				        	System.out.println("Server received c2 command - API");
+				    		clientOutput.println("Processing c2 command - API");
+
+				    		Thread.sleep(10000); //task time simulation
+
+				    		int num = new Random().nextInt(100) + 1; //number from 1 to 100
+				    		System.out.println("Server finished c2 command - API");
+				    		return Integer.toString(num);
+				        });
+	
+				        this.threadPool.execute(() -> {
+				        	System.out.println("Waiting results from API and DB");
+
+				            try {
+				                String apiData = apiResponse.get(20, TimeUnit.SECONDS); //Timeout limit
+				                String dbData = dbResponse.get(20, TimeUnit.SECONDS);
+
+				                clientOutput.println("c2 Results: "
+				                        + apiData + ", " + dbData);
+
+				            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+
+				                System.out.println("Timeout: Cancelando a execução do comando c2");
+
+				                apiResponse.cancel(true);
+				                dbResponse.cancel(true);
+				                
+				                clientOutput.println("c2 request timeout");
+				            }
+
+				            System.out.println("Server finished c2 results");
+				        });
+				        
+						break;
+					}
+					
+					case "stop": {
+						clientOutput.println("Shutting down server");
+						server.stop();
+						return;
+					}
+					
+					default: {
+						clientOutput.println("Command not found");
+					}
+				}
+				
+			}
+			
+			clientInput.close();
+			clientOutput.close();
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+
+	}
+
+}
